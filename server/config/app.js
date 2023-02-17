88d5333f49a5a45353b4b2ec41322eddd82c8fe9
Amanda Ike <amandayurike@gmail.com>
2023-02-17 17:22:13 -0500

    creation of server folder and organization

--- server/config/app.js
+++ server/config/app.js
@@ -38,8 +38,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.join(__dirname, '../node_modules')));
+app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
